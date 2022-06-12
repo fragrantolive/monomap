3811f6d267bea69d54a03b22b5a76c383f27914e
nakajimashigusa <fragrantolive2005@gmail.com>
2022-06-12 19:46:08 +0900

    tabbarの実装（仮）

--- monomap/ViewController2.swift
+++ monomap/ViewController2.swift
@@ -17,10 +17,45 @@ class ViewController2: UIViewController {
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+//    var positionY: CGFloat!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        let titles = ["月","火","水","木","金","土","日"] //タブのタイトル
+
+        //タブの横幅
+        let tabLabelWidth:CGFloat = 100
+        //タブの縦幅(UIScrollViewと一緒にします)
+        let tabLabelHeight:CGFloat = scrollView.frame.height
+
+        //タブのx座標．0から始まり，少しずつずらしていく．
+        var originX:CGFloat = 0
+        //titlesで定義したタブを1つずつ用意していく
+        for title in titles {
+            //タブになるUILabelを作る
+            let label = UILabel()
+            label.textAlignment = .center
+            label.frame = CGRect(x:originX, y:0, width:tabLabelWidth, height:tabLabelHeight)
+            label.text = title
+
+            //scrollViewにぺたっとする
+            scrollView.addSubview(label)
+
+            //次のタブのx座標を用意する
+            originX += tabLabelWidth
+        }
+
+        //scrollViewのcontentSizeを，タブ全体のサイズに合わせてあげる(ここ重要！)
+        //最終的なoriginX = タブ全体の横幅 になります
+        scrollView.contentSize = CGSize(width:originX, height:tabLabelHeight)
+
     }
+    
+   
+      
     
     override func viewDidLayoutSubviews() {
            super.viewDidLayoutSubviews()
@@ -47,3 +82,14 @@ class ViewController2: UIViewController {
         delegate?.switchMarker(type: "vendingmachine")
     }
 }
+
+
+//extension ViewController2: UIScrollViewDelegate {
+//    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+//        positionY = scrollView.contentOffset.y
+//    }
+//
+//    func scrollViewDidScroll(scrollView: UIScrollView) {
+//        scrollView.contentOffset.y = positionY
+//    }
+//}
