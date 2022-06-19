bb397613f8174d637d84444ad77042ef68941cfe
nakajimashigusa <fragrantolive2005@gmail.com>
2022-06-19 12:14:25 +0900

    scrollの実装

--- monomap/ViewController2.swift
+++ monomap/ViewController2.swift
@@ -24,33 +24,33 @@ class ViewController2: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let titles = ["月","火","水","木","金","土","日"] //タブのタイトル
+//        let titles = ["月","火","水","木","金","土","日"] //タブのタイトル
+//
+//        //タブの横幅
+//        let tabLabelWidth:CGFloat = 100
+//        //タブの縦幅(UIScrollViewと一緒にします)
+//        let tabLabelHeight:CGFloat = scrollView.frame.height
+//
+//        //タブのx座標．0から始まり，少しずつずらしていく．
+//        var originX:CGFloat = 0
+//        //titlesで定義したタブを1つずつ用意していく
+//        for title in titles {
+//            //タブになるUILabelを作る
+//            let label = UILabel()
+//            label.textAlignment = .center
+//            label.frame = CGRect(x:originX, y:0, width:tabLabelWidth, height:tabLabelHeight)
+//            label.text = title
+//
+//            //scrollViewにぺたっとする
+//            scrollView.addSubview(label)
+//
+//            //次のタブのx座標を用意する
+//            originX += tabLabelWidth
+//        }
 
-        //タブの横幅
-        let tabLabelWidth:CGFloat = 100
-        //タブの縦幅(UIScrollViewと一緒にします)
-        let tabLabelHeight:CGFloat = scrollView.frame.height
-
-        //タブのx座標．0から始まり，少しずつずらしていく．
-        var originX:CGFloat = 0
-        //titlesで定義したタブを1つずつ用意していく
-        for title in titles {
-            //タブになるUILabelを作る
-            let label = UILabel()
-            label.textAlignment = .center
-            label.frame = CGRect(x:originX, y:0, width:tabLabelWidth, height:tabLabelHeight)
-            label.text = title
-
-            //scrollViewにぺたっとする
-            scrollView.addSubview(label)
-
-            //次のタブのx座標を用意する
-            originX += tabLabelWidth
-        }
-
-        //scrollViewのcontentSizeを，タブ全体のサイズに合わせてあげる(ここ重要！)
-        //最終的なoriginX = タブ全体の横幅 になります
-        scrollView.contentSize = CGSize(width:originX, height:tabLabelHeight)
+//        //scrollViewのcontentSizeを，タブ全体のサイズに合わせてあげる(ここ重要！)
+//        //最終的なoriginX = タブ全体の横幅 になります
+//        scrollView.contentSize = CGSize(width:originX, height:tabLabelHeight)
 
     }
     
